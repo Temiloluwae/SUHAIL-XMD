c7d8148f2b17d94f7298d64815381ff0bd3bcaf4
Temiloluwae <161668702+Temiloluwae@users.noreply.github.com>
2024-10-05 08:26:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -76,8 +76,8 @@ module.exports = {
  
   author : process.env.PACK_AUTHER|| "",
   packname: process.env.PACK_NAME || "",
-  botname : process.env.BOT_NAME  || "Suhail-XMD",
-  ownername:process.env.OWNER_NAME|| "Suhail-X",
+  botname : process.env.BOT_NAME  || "Smile",
+  ownername:process.env.OWNER_NAME|| "Smile",
 
 
   errorChat : process.env.ERROR_CHAT || "",
